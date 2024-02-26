fc1c0c469453880577fa3267575accdf2d32eed7
ChaoukiBehaz <behazchaouki@gmail.com>
2024-02-26 19:52:31 +0100

    Initial commit

---
+++ src/main/java/org/example/Exo1.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Exo1 {
+    public Exo1() {
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            throw new NullPointerException("String must not be null");
+        } else {
+            s = s.toLowerCase().replaceAll("\\s+", "");
+            int i = 0;
+
+            for(int j = s.length() - 1; i < j; --i) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    return false;
+                }
+
+                ++j;
+            }
+
+            return true;
+        }
+    }
+}
